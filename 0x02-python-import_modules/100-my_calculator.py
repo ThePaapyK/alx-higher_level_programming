814a9c03a46da5891e02184d20390b7f824ce98c
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-08-25 20:39:42 +0000

    Hawayew

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,34 +2,16 @@
 
 if __name__ == "__main__":
     from calculator_1 import add, div, mul, sub
-    from sys import argv
+    from sys import argv, exit
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    gen = ['+', '-', '*', '/']
-    s = 0
-    for i in range(len(gen)):
-        if (argv[2] == gen[i]):
-            s = -1
-            break
-        else:
-            s = s + 1
-
-    if s >= 4:
+    sign = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(sign.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
-        a = int(argv[1])
-        b = int(argv[3])
-
-        if (s == -1):
-            if (argv[2] == '+'):
-                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-            elif (argv[2] == '-'):
-                print("{:d} * {:d} = {:d}".format(a, b, sub(a, b)))
-            elif (argv[2] == '*'):
-                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-            elif (argv[2] == '/'):
-                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, argv[2], b, sign[argv[2]](a, b)))
