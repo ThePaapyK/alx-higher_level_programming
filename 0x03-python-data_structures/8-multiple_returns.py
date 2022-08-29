2e23a3d5d5ca36499e6977a90210faac6e685e72
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-08-29 18:54:12 +0000

    Halo

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if len(sentence) <= 0:
         return None
-
-    rets = len(sentence), sentence[0]
-    return rets
+    else:
+        return len(sentence), sentence[0]
