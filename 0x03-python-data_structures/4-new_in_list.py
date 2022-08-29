2c7d8409aae35939ae28a268bd1daf657168c3dc
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-08-29 18:30:03 +0000

    ese ebaadze

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
-        join = my_list
+        join = my_list[:]
         if 0 <= idx < len(my_list):
             join[idx] = element
     return join
