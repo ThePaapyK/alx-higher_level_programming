1f70f2d92a7489df1f377135f9b7554cd84882ae
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-10-06 18:12:23 +0000

    Solved pycodestyle

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def number_keys(a_dictionary):
     new_l = list(a_dictionary)
     return (len(new_l))
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def print_sorted_dictionary(a_dictionary):
     new_l = sorted(a_dictionary)
     for i in new_l:
