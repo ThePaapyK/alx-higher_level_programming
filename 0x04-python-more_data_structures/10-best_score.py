749fa37d18aaef2d6c056a20db5f12c3fd679fcb
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-10-06 18:25:45 +0000

    Issues with task 10 resolved

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,13 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return (None)
 
-    return (max(a_dictionary))
+    fir = list(a_dictionary)
+    st = fir[0]
+    tem = a_dictionary[st]
+
+    for k, v in a_dictionary.items():
+        if v > tem:
+            tem = v
+            st = k
+
+    return (st)
