374b3f8fb69f3c9c30f86d383f85941362e1b00b
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-10-08 10:56:33 +0000

    Thats allll

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    new = list(a_dictionary)
+    for i in new:
+        if a_dictionary[i] == value:
+            del a_dictionary[i]
+
+    return (a_dictionary)
