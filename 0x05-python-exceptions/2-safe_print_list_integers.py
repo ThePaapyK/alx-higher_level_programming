d80f0ada0238742d60e12f347fd987b1508e756b
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-10-08 18:14:10 +0000

    Exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,8 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
 
         except (TypeError, ValueError):
             continue
-        except (IndexError):
-            break
 
     print("")
     return (num)
