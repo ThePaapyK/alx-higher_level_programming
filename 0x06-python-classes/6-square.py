bb1c7809d5340b7d0304da7d941f0da9e783f852
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-10-22 20:25:35 +0000

    It works now

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,10 +48,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-              len(value) != 2 or
-              not all(isinstance(i, int) for i in value) or
-              not all(i >= 0 for i in value)):
+        if not isinstance(value, tuple) or \
+               len(value) != 2 or \
+               not all(isinstance(i, int) for i in value) or \
+               not all(i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -72,7 +72,7 @@ class Square:
 
             for i in range(self.__size):
                 for m in range(self.__position[0]):
-                    print(" ", end ="")
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print("")
