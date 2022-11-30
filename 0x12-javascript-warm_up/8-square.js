026df7a00012583a0cf0339a6669941ec487eb94
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-11-30 01:30:02 +0000

    Jesus

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,17 +2,18 @@
 
 let mySq = '';
 const num = parseInt(process.argv[2]);
-
-if (isNaN(num)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < num; i++) {
-    for (let j = 0; j < num; j++) {
-      mySq += 'x';
-    } 
-    if (i < (num - 1)) {
-      mySq += '\n';
+if (num > 0) {
+  if (isNaN(num)) {
+    console.log('Missing size');
+  } else {
+    for (let i = 0; i < num; i++) {
+      for (let j = 0; j < num; j++) {
+        mySq += 'x';
+      } 
+      if (i < (num - 1)) {
+        mySq += '\n';
+      }
     }
+    console.log(mySq);
   }
-  console.log(mySq);
 }
