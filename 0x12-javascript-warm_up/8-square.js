86e7b5ebd1fde049d5182b6d07802516334ff82d
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-11-30 01:02:44 +0000

    L'olam

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 let mySq = '';
 const num = parseInt(process.argv[2]);
 
-if (isNaN(num)) {
+if (isNaN(num) || num <= 0) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
