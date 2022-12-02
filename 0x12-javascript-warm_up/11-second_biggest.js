6eb5a345bd1283d39387dff6e08b7b771ceb30cb
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-02 12:49:47 +0000

    Howdey

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,6 +8,6 @@ if (len === 2 || len === 3) {
   for (let i = 0; i < (len - 2); i++) {
     myArr[i] = parseInt(process.argv[i + 2]);
   }
-  myArr.sort(function(a, b){return b-a});
+  myArr.sort(function (a, b) {return b - a });
   console.log(myArr[1]);
 }
