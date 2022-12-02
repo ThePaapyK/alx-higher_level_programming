940ceb61568c1320abd3a3edaa7da8d19e73529e
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-02 11:44:37 +0000

    herh

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,18 +2,17 @@
 
 let mySq = '';
 const num = parseInt(process.argv[2]);
-if (num > 0) {
-  if (isNaN(num)) {
-    console.log('Missing size');
-  } else {
-    for (let i = 0; i < num; i++) {
-      for (let j = 0; j < num; j++) {
-        mySq += 'x';
-      } 
-      if (i < (num - 1)) {
-        mySq += '\n';
-      }
+
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      mySq += 'X';
+    } 
+    if (i < (num - 1)) {
+      mySq += '\n';
     }
-    console.log(mySq);
   }
+  console.log(mySq);
 }
