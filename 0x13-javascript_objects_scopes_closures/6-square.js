14c455a58317cf839f836056b3fda05b8308c96b
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-03 19:21:02 +0000

    KS

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const SquareIt = require('./5-square.js');
+
+class Square extends SquareIt {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+	console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
