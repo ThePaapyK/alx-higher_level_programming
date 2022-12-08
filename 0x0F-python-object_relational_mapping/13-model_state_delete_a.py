ad5ea7f07c77fc3c7a8c23c5652371f677754081
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-08 22:56:58 +0000

    King

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Uses SQLAlchemy. Adds the State object \
+“Louisiana” to the database hbtn_0e_6_usa
+takes 3 arguments: mysql username, mysql password and database name.
+"""
+
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
+                             3306/{}'.format(sys.argv[1], sys.argv[2],
+                                             sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    for state in session.query(State).filter(State.name.like('%a%')):
+        session.delete(state)
+    session.commit()
+    session.close()
