8aefa067be002ec9969204ab7686602e8e587245
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-08 17:41:06 +0000

    Help me

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,15 +2,16 @@
 """ Uses SQLAlchemy.  a python file that contains the class
 definition of a State and an instance Base = declarative_base():
     State class:
-    inherits from Base Tips
+    inherits from Base
     links to the MySQL table states
     class attribute id that represents a column of an auto-generated, \
-            unique integer, can’t be null and is a primary key
+unique integer, can’t be null and is a primary key
     class attribute name that represents a column of a string \
-            with maximum 128 characters and can’t be null
+with maximum 128 characters and can’t be null
     You must use the module SQLAlchemy
     Your script should connect to a MySQL server running \
-            on localhost at port 3306 """
+on localhost at port 3306
+"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
@@ -19,6 +20,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ State class that links to state table in database"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
