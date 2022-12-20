312c56acee0c4bad7c46419043adfb70fe0cab81
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-20 15:18:10 +0000

    ai

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in URL, send POST request setting some params, display body
-curl -sX POST -d "email=test@gmail.com&subject=I will always be there for PLD" "$1"
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I will always be there for PLD"
