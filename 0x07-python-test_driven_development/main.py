840ceb9eb27760e493d9f89537c69d464af007ef
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-23 17:00:46 +0000

    happy

---
+++ 0x07-python-test_driven_development/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+            [1 ,2 ],
+                ['e', 8, 10]
+                ]
+print(matrix_divided(matrix, 2.3))
+print(matrix)
