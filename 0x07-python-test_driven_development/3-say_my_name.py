f611c45d96a4d5ca486bb96be41f8816ef63e865
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-24 12:32:02 +0000

    Third task

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """  prints My name is <first name> <last name>"""
 
+
 def say_my_name(first_name, last_name=""):
+    """prints My name is <first name> <last name>"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
