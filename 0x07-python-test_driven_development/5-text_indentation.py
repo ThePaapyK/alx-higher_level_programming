c917ffc56a890fe3f7a2243d7d85fa2f666771e4
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-24 18:39:32 +0000

    Kody

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""prints a text with 2 new lines after each of \
+these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of \
+    these characters: ., ? and :"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    prev = ''
+    for i in text:
+        if (prev == ".") or (prev == "?") or (prev == ":"):
+            print("")
+            print("")
+        else:
+            print(i, end="")
+        prev = i
