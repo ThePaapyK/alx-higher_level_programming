2fc86969a87af00b444b1a8ece82db68f04ae1a4
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-12-24 12:59:14 +0000

    God is good

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""prints a square with the character #"""
+
+
+def print_square(size):
+    """"prints a square with the character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size < 0 and isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    obj = "#"
+    for i in range(size):
+        for j in range(size):
+            print(obj, end="")
+        if i < (size + 1):
+            print("")
