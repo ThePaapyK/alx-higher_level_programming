1237f8a743a7fdfa1a5cc2bbc91de89c3b6aa946
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-06 02:50:54 +0000

    github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,6 @@ from sys import argv
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     try:
-        print(result.json().get('id'))
+        print(r.json().get('id'))
     except KeyError:
         print("None")
