63021f37f818cc8221d387ec68286d5b99db59cd
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-06 01:33:47 +0000

    api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(j['id'], j['name']))
-    except requests.exceptions.JSONDecodeError:
+    except Exception:
         print("Not a valid JSON")
