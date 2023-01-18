394749a00340534a733749dcdec549bcddddf016
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-18 04:31:35 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 // reads and prints the content of a file.
 
 const fs = require('fs');
-fs.readFile(process.argv[2], function(err, data) {
-	if (err) {
-		return console.error(err);
-	}
-	console.log(data.toString());
+fs.readFile(process.argv[2], function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data.toString());
 });
