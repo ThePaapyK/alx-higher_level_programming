4b886b5e404a289d715aacf07deabaa1e8254d8c
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-18 01:21:06 +0000

    readfile

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// reads and prints the content of a file.
+
+const fs = require('fs');
+fs.readFile(process.argv[2], function(err, data) {
+	if (err) {
+		return console.error(err);
+	}
+	console.log(data.toString());
+});
