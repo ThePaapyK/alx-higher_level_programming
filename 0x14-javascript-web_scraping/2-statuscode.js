ee71bce0db33f5985526a8d570a4fb970ab890ff
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-18 05:19:32 +0000

    get request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   console.log('code:', response && response.statusCode);
 });
